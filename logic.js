0ef0c6f88edf6f6a238f14b920a22356a89fe96e
Jeff C <50182734+jephrocs@users.noreply.github.com>
2020-09-13 18:08:29 -0700

    changed logic

--- logic.js
+++ logic.js
@@ -6,12 +6,18 @@ const lobby = (players) => {
     }
 }
 
+const scores = {
+
+    
+}
 
 
 
 
 
 const rpsMatch = (playername1, playername2, choice1, choice2) => {
+    var score1 = 0
+    var score2 = 0
     var player1 = {
         name: playername1,
         choice: choice1,
@@ -37,10 +43,10 @@ const rpsMatch = (playername1, playername2, choice1, choice2) => {
         return 'critical error check choice'
     }
     if (rps(choice1, choice2) === 'Its a tie') {
-        return choice1 +' ' + player1.name + ' and ' + player2.name + ' you have dissapointed me with a stalemate ' + choice2 
+        return choice1 + ' ' + player1.name + ' and ' + player2.name + ' you have dissapointed me with a stalemate ' + choice2
     }
 
-    return rps(choice1, choice2) === 'player 1 wins' ? player1.name + ' wins with ' + choice1 : player2.name + ' wins with ' + choice2
+    return rps(choice1, choice2) === 'player 1 wins' ? player1.name + ' wins with ' + choice1 + ' beats ' + choice2 + ' ' + ++score1 : player2.name + ' wins with ' + choice2 + ' beats ' + choice1 + ' ' + ++score2
 }
 
 const rpsRan = () => {
@@ -56,13 +62,33 @@ const rpsRan = () => {
             break;
     }
 }
-// console.log(rpsRan())
-console.log(rpsMatch('marle', 'crono', rpsRan(), rpsRan()))
-// console.log(rpsMatch('marle', 'crono', 'rock', 'paper'))
-// console.log(rpsMatch('marle', 'crono', 'rock', 'scissors'))
-// console.log(rpsMatch('marle', 'crono', 'paper', 'scissors'))
-// console.log(rpsMatch('marle', 'crono', 'scissors', 'scissors'))
-// console.log(rpsMatch('marle', 'crono', 'rock', 'asdf'))
 
-// console.log(rpsMatch('marle', 'crono', 'asdf', 'asdf'))
-// console.log(rpsMatch('marle', 'crono', 'asdf', 'paper'))
+
+
+const nameRan = () => {
+    switch (Math.floor(Math.random() * 7)) {
+        case 0:
+            return "Crono";
+            break;
+        case 1:
+            return "Marle";
+            break;
+        case 2:
+            return "Lucca";
+            break;
+        case 3:
+            return "Glenn";
+            break;
+        case 4:
+            return "Ayla";
+            break;
+        case 5:
+            return "Robo";
+            break;
+        case 6:
+            return "Magus";
+            break;
+    }
+}
+
+console.log(rpsMatch(nameRan(), nameRan(), rpsRan(), rpsRan()))
